7eaeba28157fda54cd8b2a5f6165f955bfe78ffe
kairbekov <kairbekov.official@gmail.com>
2025-06-24 16:48:03 +0500

    news done

--- pages/news_detail.js
+++ pages/news_detail.js
@@ -5,18 +5,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('news-content');
     const article = articles.find(a => a.id === articleId);
 
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const options = { day: 'numeric', month: 'long', year: 'numeric' };
+        if (document.documentElement.lang === 'kz') {
+            return date.toLocaleDateString('kz-KZ', options);
+        }
+        if (document.documentElement.lang === 'ru') {
+            return date.toLocaleDateString('ru-RU', options);
+        }
+    }
+
     if (article) {
         container.innerHTML = `
             <div class="news-detail-item">
                 <a href="news.html" class="back-link">Назад к новостям</a>
                 <div class="news-detail-header">
-                    <p><em>${article.date}</em></p>
+                    <p class="news-date">${formatDate(article.date)}</p>
                 </div>
-                <img class="news-detail-img" src="../../${article.image}" alt="${article.title}" />
+                <img class="news-detail-img" src="../../img/news/${article.image}" alt="${article.title}" />
                 <div>${marked.parse(article.content)}</div>
             </div>
         `;
     } else {
-        container.innerHTML = '<p>Новость не найдена</p>';
+        if (document.documentElement.lang === 'kz') {
+            container.innerHTML = '<p>Жаңалық табылмады</p>';
+        }
+        if (document.documentElement.lang === 'ru') {
+            container.innerHTML = '<p>Новость не найдена</p>';
+        }
     }
 });
--- script.js
+++ script.js
@@ -21,6 +21,49 @@ document.addEventListener('DOMContentLoaded', function () {
             myMap.geoObjects.add(placemark);
         });
     });
+
+    // Массив статей
+    const newsList = document.getElementById('news-list');
+
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const options = { day: 'numeric', month: 'long', year: 'numeric' };
+        if (document.documentElement.lang === 'kz') {
+            return date.toLocaleDateString('kz-KZ', options);
+        }
+        if (document.documentElement.lang === 'ru') {
+            return date.toLocaleDateString('ru-RU', options);
+        }
+    }
+
+    if (document.documentElement.lang === 'kz') {
+        articles.forEach(article => {
+            const card = document.createElement('div');
+            card.className = 'news-item';
+            card.innerHTML = `
+            <img class="news-item-img" src="img/news/${article.image}" alt="${article.title}" />
+            <h3>${article.title}</h3>
+            <p class="news-date">${formatDate(article.date)}</p>
+            <p>${article.summary}</p>
+            <a href="pages/kz/news_detail.html?id=${article.id}" class="read-more">Толығырақ оқыңыз</a>
+        `;
+            newsList.appendChild(card);
+        });
+    }
+    if (document.documentElement.lang === 'ru') {
+        articles.forEach(article => {
+            const card = document.createElement('div');
+            card.className = 'news-item';
+            card.innerHTML = `
+            <img class="news-item-img" src="img/news/${article.image}" alt="${article.title}" />
+            <h3>${article.title}</h3>
+            <p class="news-date">${formatDate(article.date)}</p>
+            <p>${article.summary}</p>
+            <a href="pages/ru/news_detail.html?id=${article.id}" class="read-more">Читать далее</a>
+        `;
+            newsList.appendChild(card);
+        });
+    }
 });
 
 // header
@@ -116,3 +159,5 @@ window.addEventListener('resize', () => {
 // Старт
 changeBackground(); // Установить первый фон сразу
 setInterval(changeBackground, 8000); // Менять фон каждые 8 секунд
+
+
